9c1fe570f6609f3c219556401d729309bbe782cc
Rihan Syed <rihan.syed2025@gmail.com>
2026-03-04 06:13:38 +0530

    /api/v2

--- Metallisense-frontend/src/services/opcService.js
+++ Metallisense-frontend/src/services/opcService.js
@@ -1,13 +1,13 @@
 import api from "./api";
 
 // OPC Status
-export const getOPCStatus = () => api.get("/api/v2/synthetic/opc-status");
+export const getOPCStatus = () => api.get("/synthetic/opc-status");
 
 // OPC Connect
-export const connectOPC = () => api.post("/api/v2/synthetic/opc-connect");
+export const connectOPC = () => api.post("/synthetic/opc-connect");
 
 // OPC Disconnect
-export const disconnectOPC = () => api.post("/api/v2/synthetic/opc-disconnect");
+export const disconnectOPC = () => api.post("/synthetic/opc-disconnect");
 
 // Generate Synthetic Reading
 export const generateSyntheticReading = (data) =>
